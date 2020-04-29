8f54c80b7478f4694e8db1ef4d90fa72d2beb6cd
celine haddad <rashedmajedrabadi@gmail.com>
2020-04-29 06:08:59 +0300

    the app is added to github

--- app/src/main/java/just/edu/cit/se/medcart/lAdapter.java
+++ app/src/main/java/just/edu/cit/se/medcart/lAdapter.java
@@ -4,15 +4,20 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class lAdapter extends RecyclerView.Adapter<lAdapter.CartViewHolder>{
     private Context mCtx;
+    private ArrayList<String> List;
 
-    public lAdapter(Context mCtx) {
+    public lAdapter(Context mCtx, ArrayList<String> list) {
         this.mCtx = mCtx;
+        List = list;
     }
 
     @NonNull
@@ -24,10 +29,8 @@ public class lAdapter extends RecyclerView.Adapter<lAdapter.CartViewHolder>{
 
     @Override
     public void onBindViewHolder(@NonNull CartViewHolder holder, int position) {
-
+        holder.name.setText("celine");
     }
-
-
 
     @Override
     public int getItemCount() {
@@ -35,8 +38,10 @@ public class lAdapter extends RecyclerView.Adapter<lAdapter.CartViewHolder>{
     }
 
     public class CartViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
         public CartViewHolder(View view) {
             super(view);
+            name = itemView.findViewById(R.id.loc);
         }
     }
 }
--- app/src/main/java/just/edu/cit/se/medcart/locations.java
+++ app/src/main/java/just/edu/cit/se/medcart/locations.java
@@ -7,10 +7,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 public class locations extends AppCompatActivity {
 
     RecyclerView recyclerView;
-    public static cartAdapter CA;
+    public static lAdapter LA;
+    public static ArrayList<String> list;
+
 
 
 
@@ -19,9 +23,19 @@ public class locations extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locations);
 
+        list = new ArrayList<>();
+        LA=new lAdapter(this,list);
         recyclerView = findViewById(R.id.locations);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(LA);
+
+
+
+        list.add("celo");
+
+        LA.notifyDataSetChanged();
+
 
 
     }
